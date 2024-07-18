f4e11a4785bc370bbcbaf9daddfce1e67b9d214a
Juani <juan.seijas@fleetbooster.net>
2024-07-17 22:41:09 -0300

    Add atuth

--- src/components/LayoutDashboard.tsx
+++ src/components/LayoutDashboard.tsx
@@ -1,5 +1,5 @@
 import useWindowResize from "@/hooks/useWindowResize";
-import React from "react";
+import type React from "react";
 import useToggle from "@/hooks/useToggle";
 import Drawer from "react-modern-drawer";
 import DrawerContent from "./DrawerContent";
@@ -17,19 +17,19 @@ interface LayoutDashboardProps {
 export default function LayoutDashboard({ children }: LayoutDashboardProps) {
 	const { isDesktop, isMobile } = useWindowResize();
 	const { state, toggle } = useToggle(false);
-	// const { user, isLoading } = useAuth();
+	const { user, isLoading } = useAuth();
 
-	// if (isLoading) {
-	// 	return (
-	// 		<div className="h-screen w-full justify-center items-center flex flex-col bg-primary zoom-in-out ">
-	// 			<img
-	// 				src="/logo.png"
-	// 				alt="Logo"
-	// 				className="w-full h-20 object-contain"
-	// 			/>
-	// 		</div>
-	// 	);
-	// }
+	if (isLoading) {
+		return (
+			<div className="h-screen w-full justify-center items-center flex flex-col bg-primary zoom-in-out ">
+				<img
+					src="/logo.png"
+					alt="Logo"
+					className="w-full h-20 object-contain"
+				/>
+			</div>
+		);
+	}
 
 	return (
 		<div className="w-full">
@@ -47,7 +47,7 @@ export default function LayoutDashboard({ children }: LayoutDashboardProps) {
 					/>
 				</div>
 				<div>
-					{/* {!isLoading && user && isDesktop && (
+					{!isLoading && user && isDesktop && (
 						<User
 							className="text-white"
 							avatarProps={{
@@ -59,31 +59,10 @@ export default function LayoutDashboard({ children }: LayoutDashboardProps) {
 									{user.email}
 								</span>
 							}
-						/> */}
-					{/* )} */}
+						/>
+					)}
 				</div>
 			</div>
-			{/* {isDesktop && (
-				<div
-					style={{
-						position: "relative",
-						display: "flex",
-						minHeight: "calc(100vh - 5rem)",
-					}}
-				>
-					<div className="min-h-full bg-gray-200 w-[20%] border-2 ">
-						<DrawerContent />
-					</div>
-					<div
-						style={{
-							position: "relative",
-						}}
-						className="w-[80%]"
-					>
-						{children}
-					</div>
-				</div>
-			)} */}
 			<>
 				<Drawer
 					open={state}
@@ -97,7 +76,7 @@ export default function LayoutDashboard({ children }: LayoutDashboardProps) {
 							className="text-2xl text-primary cursor-pointer"
 						/>
 					</div>
-					{/* <div className="px-4 pt-8">
+					<div className="px-4 pt-8">
 						{!isLoading && user && isMobile && (
 							<User
 								className="text-black text-xs"
@@ -112,14 +91,12 @@ export default function LayoutDashboard({ children }: LayoutDashboardProps) {
 								}
 							/>
 						)}
-					</div> */}
+					</div>
 
 					<DrawerContent />
 				</Drawer>
 				<div className="w-full">{children}</div>
 			</>
-			{/* {/* */}
-			{/*  */} {/* {children} */}
 		</div>
 	);
 }
