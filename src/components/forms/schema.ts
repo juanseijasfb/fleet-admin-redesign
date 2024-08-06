341a99957f311f8a89fd304569043125bcee84c1
Santiago Maza <santiagomaza33@gmail.com>
2024-08-06 02:43:03 -0300

    added validate maxLoadWeight

--- src/components/forms/schema.ts
+++ src/components/forms/schema.ts
@@ -7,7 +7,7 @@ export const driverSchema = Yup.object().shape({
 	mcNumber: Yup.number().required("MC Number is required"),
 	carrierName: Yup.string().required("Carrier Name is required"),
 	trailerType: Yup.string().required("Trailer Type is required"),
-	maxLoadWeight: Yup.number().required("Max Load Weight is required"),
+	maxLoadWeight: Yup.number().min(0, 'Max Load Weight must be greater than or equal to 0').max(99999, 'Max Load Weight must be less than or equal to 99999').required("Max Load Weight is required"),
 });
 export const driverUpdateSchema = Yup.object().shape({
 	firstName: Yup.string().required("First name is required"),
@@ -16,7 +16,7 @@ export const driverUpdateSchema = Yup.object().shape({
 	mcNumber: Yup.number().required("MC Number is required"),
 	carrier: Yup.string().required("Carrier Name is required"),
 	equipment: Yup.string().required("Trailer Type is required"),
-	weight: Yup.number().required("Max Load Weight is required"),
+	weight: Yup.number().min(0, 'Max Load Weight must be greater than or equal to 0').max(99999, 'Max Load Weight must be less than or equal to 99999').required("Max Load Weight is required"),
 });
 export const dispatcherSchema = Yup.object().shape({
 	firstName: Yup.string().required("First name is required"),
